c1dde03d1bd1c477c9de6c8410438e9ae0c855d3
Yunjuniverse <theuniversejun@gmail.com>
2022-08-08 14:23:19 +0900

    first

---
+++ src/test/java/com/codestates/practice_github_action_deploy/PracticeGithubActionDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practice_github_action_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
